2a5c64a2c363b2a4fe01685a01233a17700ccc8c
zachary mines <zmines@wesleyan.edu>
2020-06-18 18:05:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,10 @@ def countdown (number)
 end
 
 def countdown_with_sleep (number)
+  while number >= 0
+      puts "#{number} SECOND(S)!"
+      number -= 1
+      sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
